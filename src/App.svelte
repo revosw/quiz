e20220ba125e586717b2ca375276c74de582e98a
revosw <simenwiik@hotmail.com>
2023-01-08 00:29:43 +0100

    add socket-io

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
+  import Socket from "socket.io";
   import Admin from "./Admin.svelte";
   import Home from "./Home.svelte";
-import Row from "./lib/flex/Row.svelte";
+  import Row from "./lib/flex/Row.svelte";
   import Question from "./Question.svelte";
   import Results from "./Results.svelte";
+  import io from "socket.io-client";
 
-  let page = "/"
+  let page = "/";
+  const socket = io("http://localhost:3000");
 </script>
 
 <main>
   <Row>
-    <button on:click={() => page = "/"}>/</button>
-    <button on:click={() => page = "/question"}>question</button>
-    <button on:click={() => page = "/results"}>results</button>
-    <button on:click={() => page = "/admin"}>admin</button>
+    <button on:click={() => (page = "/")}>/</button>
+    <button on:click={() => (page = "/question")}>question</button>
+    <button on:click={() => (page = "/results")}>results</button>
+    <button on:click={() => (page = "/admin")}>admin</button>
   </Row>
   {#if page === "/"}
     <Home />
